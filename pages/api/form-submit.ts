0d3dfd058dd6cc91a9cb2119818f807dc3b7b5ca
Magicpig55 <kelott5566@gmail.com>
2023-01-01 18:51:03 -0700

    Comments

--- pages/api/form-submit.ts
+++ pages/api/form-submit.ts
@@ -2,22 +2,29 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 /** Really basic back end handler for a form input. Includes some basic validation*/
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Only allow POST
     if(req.method === 'POST') {
         try {
+            // Parse given input
             const data = JSON.parse(req.body);
             
+            // Validate email
             const email: string | undefined = data['email'];
             if(typeof email !== 'string') return res.status(400).send("Email must be a string");
             if(email.length === 0) return res.status(400).send("Email must not be empty");
             
+            // Validate radio input
             const radioValue: boolean | undefined = data['radioValue'];
             if(typeof radioValue !== 'boolean') return res.status(400).send("radioValue must be a boolean");
 
+            // Logging is cool
             console.log("Email", email);
             console.log("radioValue", radioValue);
 
+            // ok
             res.status(200).send('ok')
         } catch (e) {
+            // Oops couldn't parse the input
             console.error(e);
             res.status(400).send('invalid argument');
         }
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,23 +2,32 @@ import { FormEventHandler, useState } from "react";
 
 export default function Contact() {
 
-    
+    /** Form data! libraries would be cool, but pure react works just fine */
     const [email, setEmail] = useState('');
     const [radioValue, setRadioValue] = useState(false);
     const [serverResult, setServerResult] = useState<null | string>(null);
     
+    /** Handles form submission */
     const handleForm: FormEventHandler = async (ev) => {
-        ev.preventDefault()
+        ev.preventDefault() // Disable default behaviour, though it's set up correctly. We aren't returning anything helpful
+        
+        // Make data
         const data = JSON.stringify({email, radioValue});
+
+        // Submit
         const res = await (await fetch('/api/form-submit', {method: "POST", body: data})).text();
+
+        // Cool, don't care about the response so let's just display it
         console.log(`Server returned a response: ${res}`);
         setServerResult(res);
+
         //* Disabling this for now, not sure about two-way binding with radio buttons atm
         // setEmail("");
         // setRadioValue(false)
     }
 
     function ServerResult() {
+        // Display the result somehow
         if(serverResult) return (<div>{serverResult}</div>)
         else return (<></>);
     }
