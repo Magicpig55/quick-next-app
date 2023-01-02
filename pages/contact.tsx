354d155770cb2511a4f981763481f46b318655d7
Magicpig55 <kelott5566@gmail.com>
2023-01-01 18:09:32 -0700

    Contact form!

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,17 @@
 export default function Contact() {
     return (<>
         <article>
-            <h1>Contact</h1>
+            <h1>Contact (ish)</h1>
+            <form method="post">
+                <label htmlFor="form-email">Email: </label>
+                <input type="email" id="form-email" name="email" placeholder="bob@bob.com"></input>
+                <input type="radio" id="form-radio-yes" name="selection"></input>
+                <label htmlFor="form-radio-yes">Yes</label>
+                <input type="radio" id="form-radio-no" name="selection"></input>
+                <label htmlFor="form-radio-yes">No</label>
+                <br />
+                <button>Submit</button>
+            </form>
         </article>
     </>)
 }
